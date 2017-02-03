2f0f283b56a7e392e7b9681ace95b359b13319cb
gilano <olivergilan@gmail.com>
2017-02-03 11:19:05 -0500

    allIn

--- app/src/main/java/com/example/gilano/deca/allIn.java
+++ app/src/main/java/com/example/gilano/deca/allIn.java
@@ -1,13 +1,64 @@
 package com.example.gilano.deca;
 
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class allIn extends AppCompatActivity {
+
+    private ListView displayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_in);
+        final ArrayList<Student> studentsIn = new ArrayList<Student>();
+        DatabaseReference mRef = FirebaseDatabase.getInstance().getReference("students");
+        displayList = (ListView)findViewById(R.id.allInList);
+
+        mRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for(DataSnapshot child: dataSnapshot.getChildren() ){
+                    String name = child.child("name").getValue().toString();
+                    String firstName = child.child("firstName").getValue().toString();
+                    String lastName = child.child("lastName").getValue().toString();
+                    String status = child.child("status").getValue().toString();
+                    boolean stat = true;
+                    if(status == "false"){
+                        stat = false;
+                    }
+                    String id = child.child("id").getValue().toString();
+
+                    Student student = new Student(Integer.parseInt(id), firstName, lastName, stat);
+                    if(stat == true){
+                        studentsIn.add(student);
+                    }
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getMessage());
+            }
+        });
+
+        StudentAdapter listAdapter = new StudentAdapter(this, R.layout.listview_item_row, studentsIn);
+        displayList.setAdapter(listAdapter);
+
     }
 }
--- app/src/main/java/com/example/gilano/deca/barscan.java
+++ app/src/main/java/com/example/gilano/deca/barscan.java
@@ -1,5 +1,6 @@
 package com.example.gilano.deca;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -69,10 +70,24 @@ public class barscan extends AppCompatActivity implements OnClickListener{
     public void checkIn(View view){
         id = inputID.getText().toString();
         mDatabase.child("students").child(id).child("status").setValue(true);
+        inputID.setText("");
+        Context context = getApplicationContext();
+        CharSequence text = "Checked In";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 
     public void checkOut(View view) {
         id = inputID.getText().toString();
         mDatabase.child("students").child(id).child("status").setValue(false);
+        inputID.setText("");
+        Context context = getApplicationContext();
+        CharSequence text = "Checked Out";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
